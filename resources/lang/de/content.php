570a2db9a6911f6763c5581eeb2c30055790c061
Marcel Stadelmann <m@marceli.to>
2025-05-05 09:58:47 +0200

    wip

--- resources/lang/de/content.php
+++ resources/lang/de/content.php
@@ -17,7 +17,7 @@ return [
   'author_heading_2'      => 'Burkhalter Sumi Architekten',
   'author_heading_3'      => 'Marianne Burkhalter und Christian Sumi',
   'author_description_1'  => 'Yves Schihin, Partner<br>Urs Rinklef, Partner',
-  'author_description_2'  => 'Marianne Burkhalter, Partnerin / Christian Sumi, Partner<br>Yves Schihin, Partner / Urs Rinklef, Partner (2012)',
+  'author_description_2'  => 'Marianne Burkhalter, Partnerin / Christian Sumi, Partner<br>Yves Schihin, Mitinhaber und Partner (ab 2009) / Urs Rinklef, Mitinhaber und Partner (ab 2012)',
   'author_description_3'  => 'Marianne Burkhalter, Partnerin<br>Christian Sumi, Partner',
 
 ];
